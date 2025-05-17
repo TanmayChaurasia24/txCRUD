5e430a95072a819461e45a47983b0e1dfd06a59e
tanmay <tanmaykumar.kanpur@gmail.com>
2025-05-17 21:25:54 +0530

    created journal entry onchain

--- solana-counter/programs/solana-counter/src/lib.rs
+++ solana-counter/programs/solana-counter/src/lib.rs
@@ -6,11 +6,39 @@ declare_id!("3t1GKEdWt7Dnt8XzkJkHrCGtDoqyZytC4n56yoKnaK8w");
 pub mod solana_counter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_journal_entry(ctx: Context<CreateEntry>, title: String, message: String) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.owner = *ctx.accounts.owner.key;
+        journal_entry.title = title;
+        journal_entry.description = message;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(title: String)]
+pub struct CreateEntry<'info> {
+    #[account(
+        init,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        space = 8 + JournalEntryState::INIT_SPACE,
+        payer = owner
+    )]
+    pub journal_entry: Account<'info,JournalEntryState>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState {
+    pub owner: Pubkey,
+    #[max_len(50)]
+    pub title: String,
+    #[max_len(1000)]
+    pub description: String,
+}
